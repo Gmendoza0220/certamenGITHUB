525ba0246bfc5579a25508715591a264bf5d2566
Gmendoza0220 <geramendozar24@gmail.com>
2024-11-12 15:51:44 -0300

    COMMIT 3 (FINAL) - Métodos para mostrar sugerencias + últimos detalles + Link repositorio.

--- app/src/main/java/com/example/certamengithub/MainActivity.java
+++ app/src/main/java/com/example/certamengithub/MainActivity.java
@@ -14,10 +14,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    // ENLACE A REPOSITORIO GITHUB
+    // https://github.com/Gmendoza0220/certamenGITHUB
+
     // COMMIT 1 - Interfaz de usuario, IDs y Spinner.
 
     EditText txtPeso, txtEstatura;
-    TextView lblResultadoIMC;
+    TextView lblResultadoIMC, lblSugerencias;
     Button btnCalcular;
     Spinner spGenero;
 
@@ -33,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         txtEstatura = findViewById(R.id.txtEstatura);
         btnCalcular = findViewById(R.id.btnCalcular);
         lblResultadoIMC = findViewById(R.id.lblResultadoIMC);
+        lblSugerencias = findViewById(R.id.lblSugerencias);
+
 
         // Contenido del Spinner
         generos.add("Seleccione");
@@ -61,33 +66,66 @@ public class MainActivity extends AppCompatActivity {
 
     public void calcularIMC(){
 
+        // Se extraen los datos de la interfaz
         String genero = spGenero.getSelectedItem().toString();
         String peso = txtPeso.getText().toString();
         String estatura = txtEstatura.getText().toString();
 
+        // Se validan los campos
         if(peso.isEmpty() || estatura.isEmpty() || genero.equals("Seleccione")){
             Toast.makeText(this, "Complete los campos", Toast.LENGTH_SHORT).show();
         } else {
+            // Se parsean a Double los valores extraídos de la interfaz
             double pesoValor = Double.parseDouble(peso);
             double estaturaValor = Double.parseDouble(estatura)/100.0;
 
-            double imc = pesoValor / (estaturaValor*estaturaValor);
+            // Se calcula el IMC
+            double imcValor = pesoValor / (estaturaValor*estaturaValor);
 
-            if(imc < 18.5){
+            String imc = ""+imcValor;
+            imc = imc.substring(0, 5);
+
+            if(imcValor < 18.5){ // Bajo peso
                 lblResultadoIMC.setText("Bajo peso - Delgado : "+imc);
-            } else if(imc <= 24.9) {
+                bajopeso();
+            } else if(imcValor <= 24.9) { // Aceptable
                 lblResultadoIMC.setText("Adecuado - Aceptable : "+imc);
-            } else if(imc <= 29.9){
+                pesoNormal();
+            } else if(imcValor <= 29.9){ // Sobrepeso
                 lblResultadoIMC.setText("Sobrepeso - Sobrepeso : "+imc);
-            } else if (imc <= 34.9){
+                sobrepeso_Obeso();
+            } else if (imcValor <= 34.9){ // Obesidad
                 lblResultadoIMC.setText("Obesisdad grado 1 - Obesidad : "+imc);
-            } else {
+                sobrepeso_Obeso();
+            } else { // Obesidad grado extremoooo
                 lblResultadoIMC.setText("Obesidad extrema - Obesidad : "+imc);
+                sobrepeso_Obeso();
             }
         }
     }
 
-    // COMMIT 3 - MÉTODOS DE SUGERENCIAS
+    // COMMIT 3 - Métodos de sugerencias + últimos detalles
 
+    public void sobrepeso_Obeso(){
+        lblSugerencias.setText("" +
+                "1) Haga 30 minutos de ejercicio aróbico 5 veces por semana \n" +
+                "2) Logre un equilibrio entre el consumo de calorías y la actividad física \n" +
+                "3) Limite las grasas saturadas a no mas del 7% de las calorías totales \n" +
+                "4) Tenga una alimentación baja en colesterol con carnes magras, frutas, verduras y cereales");
+    }
+
+    public void bajopeso(){
+        lblSugerencias.setText("" +
+                "1) Come con más frecuencia. Empieza poco a poco a hacer 5 a 6 comidas más pequeñas durante el día \n" +
+                "2) Elige alimentos con muchos nutrientes \n" +
+                "3) Agrega aderezos \n" +
+                "4) Prueba licuados y batisos de frutas \n" +
+                "5) Controla que bebes y cuando \n" +
+                "6) Haz ejercicio");
+    }
+
+    public void pesoNormal(){
+        lblSugerencias.setText("Felicitaciones ud mantiene una excelente alimentación");
+    }
 
 }
